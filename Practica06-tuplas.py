d163edfaaa1844b7eff6d42e8442ccad3766a2c3
Jorge-Alejandro-Castro-Amaro <72835leo@universidaddeleon.edu.mx>
2026-02-11 22:27:32 -0600

    Tuplas

---
+++ Practica06-tuplas.py
@@ -0,0 +1,21 @@
+import os
+
+os.system("cls")
+
+
+
+tupla = (1,2,3,4,5,2,3,2)
+
+print(type(tupla))
+print(tupla)
+
+print("El elemento de la tupla es: ", tupla[2])
+
+for i in tupla:
+    print(i)
+    
+    
+print("La cantidad de elementos de la tupla es: ", len(tupla))
+print("La cantidad de veces que se repite el numero 2 es: ", tupla.count(2))
+print("El indice del numero 3 es: ", tupla.index(3))
+
